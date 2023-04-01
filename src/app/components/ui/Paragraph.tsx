4e10ca16a59d50c96af5320430831482075a0687
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-31 19:49:23 -0700

    Paragraph.tsx modified with forwardRef

--- src/app/components/ui/Paragraph.tsx
+++ src/app/components/ui/Paragraph.tsx
@@ -1,5 +1,7 @@
-import { FC } from 'react';
-import { cva } from 'class-variance-authority';
+import * as React from 'react';
+import { VariantProps, cva } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
 
 const paragraphVariants = cva(
   'max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center',
@@ -17,11 +19,23 @@ const paragraphVariants = cva(
 );
 
 interface ParagraphProps
-  extends HTMLAttributes<HTMLParagraphElement>,
+  extends React.HTMLAttributes<HTMLParagraphElement>,
     VariantProps<typeof paragraphVariants> {}
 
-const Paragraph: FC<ParagraphProps> = ({}) => {
-  return <div>Paragraph</div>;
-};
+const Paragraph = React.forwardRef<HTMLParagraphElement, ParagraphProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <p
+        ref={ref}
+        {...props}
+        className={cn(paragraphVariants({ size, className }))}
+      >
+        {children}
+      </p>
+    );
+  }
+);
+
+Paragraph.displayName = 'Paragraph';
 
 export default Paragraph;
