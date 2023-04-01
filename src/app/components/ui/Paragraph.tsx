e2937158f7d8783f64180553549fb3e61b6a2656
Emiko Esther S <graceupongraceemi@gmail.com>
2023-03-31 19:36:40 -0700

    Further typescript added to Paragraph.tsx and modified page.tsx

--- src/app/components/ui/Paragraph.tsx
+++ src/app/components/ui/Paragraph.tsx
@@ -1,6 +1,24 @@
 import { FC } from 'react';
+import { cva } from 'class-variance-authority';
 
-interface ParagraphProps {}
+const paragraphVariants = cva(
+  'max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center',
+  {
+    variants: {
+      size: {
+        default: 'text-base sm:text-lg',
+        sm: 'text-sm, sm:text-base'
+      }
+    },
+    defaultVariants: {
+      size: 'default'
+    }
+  }
+);
+
+interface ParagraphProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {}
 
 const Paragraph: FC<ParagraphProps> = ({}) => {
   return <div>Paragraph</div>;
