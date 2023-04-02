9d8660d46ad5560f2479758f261d74a7b36b9bdf
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-02 00:03:56 -0700

    paths in tsconfig.json modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,5 @@ import Paragraph from './components/ui/Paragraph';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main className='bg-red-500'>
-      <Paragraph size='sm'>some text</Paragraph>
-    </main>
-  );
+  return <main className='bg-red-500'></main>;
 }
