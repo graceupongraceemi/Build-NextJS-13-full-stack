32a6d82fd6d4227ff9652b82a49a9cec8b0d3bc7
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-03 15:55:30 -0700

    In layout.tsx, styled body component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({
       lang='en'
       className={cn('bg-white text-slate-900 antialiased', inter.className)}
     >
-      <body>{children}</body>
+      <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
+        {children}
+      </body>
     </html>
   );
 }
