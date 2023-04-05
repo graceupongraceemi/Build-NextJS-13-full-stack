157ba6c04ab136108c38cd15d931725a5b830a63
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-05 16:36:21 -0700

    getServerSession from next-auth added as async in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
+import { getServerSession } from 'next-auth';
 import { FC } from 'react';
 
 interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = ({}) => {
-  return <div>Navbar</div>;
+const Navbar = async ({}) => {
+  const session = await getServerSession();
+
+  return (
+    <div className='fixed backdrop-blur-sms bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
+      <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'></div>
+    </div>
+  );
 };
 
 export default Navbar;
