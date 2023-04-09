8d3b9df9da154ec6f59793b7d98ace58193d95f9
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-08 21:40:43 -0700

    setIsLoading moved to inside try and catch

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -1,18 +1,17 @@
 'use client';
 
 import { FC, useState } from 'react';
-import Button from '@/ui/Button';
-import { signIn } from 'next-auth/react';
+import { Button } from '@/ui/Button';
+import { signOut } from 'next-auth/react';
 
 interface SignOutButtonProps {}
 
 const SignOutButton: FC<SignOutButtonProps> = ({}) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const signInWithGoogle = async () => {
-    setIsLoading(true);
-
+  const signUserOut = async () => {
     try {
+      setIsLoading(true);
       await signOut();
     } catch (error) {
       // toast({
