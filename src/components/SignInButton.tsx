dcea10d4b1323c0021900bb052ce12854969be90
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-08 21:37:04 -0700

    setIsLoading moved to inside of try and catch

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { FC, useState } from 'react';
-import Button from '@/ui/Button';
+import { Button } from '@/ui/Button';
 import { signIn } from 'next-auth/react';
 
 interface SignInButtonProps {}
@@ -10,9 +10,8 @@ const SignInButton: FC<SignInButtonProps> = ({}) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const signInWithGoogle = async () => {
-    setIsLoading(true);
-
     try {
+      setIsLoading(true);
       await signIn('google');
     } catch (error) {
       // toast({
