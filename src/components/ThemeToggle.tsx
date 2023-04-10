b08cb4aa4c82215a0ab587cce717cedfd5d1fbe7
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-10 15:16:23 -0700

    DropdownMenu component with DropdownMenuTrigger from radix-ui react-dropdowm menu added to ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,14 +1,23 @@
 'use client';
 
+import { DropdownMenu } from '@/components/ui/DropdownMenu';
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 import { useTheme } from 'next-themes';
 import { FC } from 'react';
+import { Button } from './ui/Button';
 
 interface ThemeToggleProps {}
 
 const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
   const { setTheme } = useTheme();
 
-  return <div>ThemeToggle</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button></Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
 
 export default ThemeToggle;
