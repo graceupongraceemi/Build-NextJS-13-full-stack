68db7eb448018373fff6df9ef7fe3737e1acb8f5
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-11 10:19:46 -0700

    ThemeToggle now import from ThemeToggle.tsx in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
 import { buttonVariants } from '@/ui/Button';
-import { ThemeToggle } from './ThemeToggle';
+import ThemeToggle from './ThemeToggle';
 import SignInButton from '@/components/SignInButton';
 import SignOutButton from './SignOutButton';
 
