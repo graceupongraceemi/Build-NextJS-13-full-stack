0f442861d6a3e7cc189aef1e0045a5742f347637
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-11 10:25:48 -0700

    sr-only toggle them by span added to ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -18,6 +18,7 @@ const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
         <Button variant='ghost' size='sm'>
           <Sun className='rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100' />
           <Moon className='absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100' />
+          <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
     </DropdownMenu>
