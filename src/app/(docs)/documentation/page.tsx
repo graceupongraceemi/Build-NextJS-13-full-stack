2d212e7574bb06b4cc449c57fdd2041fdbf72cc5
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-14 20:05:14 -0700

    In page.tsx of documentation, Paragraph and documentation component added. In components directory, DocumentationTabs.tsx file added

--- src/app/(docs)/documentation/page.tsx
+++ src/app/(docs)/documentation/page.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
 
 import type { Metadata } from 'next';
-import LargeHeading from '@/components/ui/LargeHeading';
+import LargeHeading from '@/ui/LargeHeading';
+import Paragraph from '@/ui/Paragraph';
+import DocumentationTabs from '@/components/DocumentationTabs';
 
 export const metadata: Metadata = {
   title: 'Similarity API | Documentation',
@@ -13,6 +15,9 @@ const page: FC = ({}) => {
     <div className='container max-w-7xl mx-auto mt-12'>
       <div className='flex flex-col items-center gap-6'>
         <LargeHeading>Making a request</LargeHeading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
       </div>
     </div>
   );
