47ddbfa4da352f8b4d9d9f8ea7c41933cc593fe0
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-15 13:36:10 -0700

    In DocumentationTabs.tsx, Tabs component in that TabsList with TabsTrigger, NodeJS and Python added

--- src/components/DocumentationTabs.tsx
+++ src/components/DocumentationTabs.tsx
@@ -1,7 +1,15 @@
+import { Tabs, TabsList, TabsTrigger } from '@/ui/Tabs';
 import { FC } from 'react';
 
 const DocumentationTabs: FC = ({}) => {
-  return <div>DocumentationTabs</div>;
+  return (
+    <Tabs defaultValue='nodejs' className='max-w-2xl w-full'>
+      <TabsList>
+        <TabsTrigger value='nodejs'>NodeJS</TabsTrigger>
+        <TabsTrigger value='python'>Python</TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
 };
 
 export default DocumentationTabs;
