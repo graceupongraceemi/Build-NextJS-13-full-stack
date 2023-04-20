768048f3a8f2745171380a6ba2d34a756a16c219
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-19 18:01:43 -0700

    In auth.ts of lib, getGoogleCredentials function added for both clientId and clientSecret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,21 @@ import { db } from '@/lib/db';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
 
+function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!clientId || clientId.length === 0) {
+    throw new Error('No clientID for google provider sest');
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('No clientSecret for google provider sest');
+  }
+
+  return { clientId, clientSecret };
+}
+
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   session: {
@@ -13,8 +28,8 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret
     })
   ]
 };
