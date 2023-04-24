79dec2022baff5dffbb71a8b4ca7364a1e9a149a
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-24 13:57:16 -0700

    createNewApiKey function with async and toast added to RequestApiKey.tsx of components

--- src/components/RequestApiKey.tsx
+++ src/components/RequestApiKey.tsx
@@ -1,8 +1,40 @@
-import { FC } from 'react';
+'use client';
 
-interface RequestApiKeyProps {}
+import { FC, FormEvent, useState } from 'react';
+import { toast } from './ui/Toast';
 
-const RequestApiKey: FC<RequestApiKeyProps> = ({}) => {
+const RequestApiKey: FC = () => {
+  const [isCreating, setIsCreating] = useState<boolean>(false);
+  const [apiKey, setApiKey] = useState<string | null>(null);
+
+  const createNewApiKey = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsCreating(true);
+
+    try {
+      const generatedApiKey = await createApiKey();
+      setApiKey(generatedApiKey);
+    } catch (err) {
+      if (err instanceof Error) {
+        toast({
+          title: 'Error',
+          message: err.message,
+          type: 'error'
+        });
+
+        return;
+      }
+
+      toast({
+        title: 'Error',
+        message: 'Something went wrong.',
+        type: 'error'
+      });
+    } finally {
+      setIsCreating(false);
+    }
+  };
+
   return <div>RequestApiKey</div>;
 };
 
