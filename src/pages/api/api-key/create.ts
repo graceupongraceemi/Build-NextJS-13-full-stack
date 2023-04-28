ede6036c696ef3f659bd003cebc1311f9650929e
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-27 21:42:36 -0700

    function handler using async , existingApiKey and createdApiKey in  with-methods.ts in api-key of pages done

--- src/pages/api/api-key/create.ts
+++ src/pages/api/api-key/create.ts
@@ -5,6 +5,7 @@ import { getServerSession } from 'next-auth';
 import { nanoid } from 'nanoid';
 import { CreateApiData } from '@/types/api';
 import { z } from 'zod';
+import { withMethods } from '@/lib/api-middlewares/with-methods';
 
 const handler = async (
   req: NextApiRequest,
