95dc9bf9a6ed6b3ca4cda10135536d91d7df48ed
Emiko Esther S <graceupongraceemi@gmail.com>
2023-04-30 22:29:28 -0700

    ApiDashboard.tsx modified with LargeHeading styling along with another paragraph div

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -2,9 +2,10 @@ import { authOptions } from '@/lib/auth';
 import { db } from '@/lib/db';
 import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
-import { FC } from 'react';
 import { formatDistance } from 'date-fns';
 import LargeHeading from './ui/LargeHeading';
+import Paragraph from './ui/Paragraph';
+import { Input } from './ui/Input';
 
 const ApiDashboard = async () => {
   const user = await getServerSession(authOptions);
@@ -34,6 +35,10 @@ const ApiDashboard = async () => {
   return (
     <div className='container flex flex-col gap-6'>
       <LargeHeading>Welcome back, {user.user.name}</LargeHeading>
+      <div className='flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center'>
+        <Paragraph>Your API key:</Paragraph>
+        <Input className='w-fit truncate' readOnly value={activeApiKey.key} />
+      </div>
     </div>
   );
 };
