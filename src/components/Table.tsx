c7f01490f23c186752572a6ae4167796478d68ed
Emiko Esther S <graceupongraceemi@gmail.com>
2023-05-01 21:24:01 -0700

    In ApiDashboard.tsx, Paragraph added.  Table.tsx is added to components directory

---
+++ src/components/Table.tsx
@@ -0,0 +1,35 @@
+import { GridColumnHeaderParams, GridColDef } from '@mui/x-data-grid';
+
+const columnsDraft: GridColDef[] = [
+  {
+    field: 'col1',
+    headerName: 'API key used',
+    width: 400,
+    renderHeader(params) {
+      return (
+        <strong className='font-semibold'>{params.colDef.headerName}</strong>
+      );
+    }
+  },
+  { field: 'col2', headerName: 'Path', width: 250 },
+  { field: 'col3', headerName: 'Recency', width: 250 },
+  { field: 'col4', headerName: 'Duration', width: 250 },
+  { field: 'col5', headerName: 'Status', width: 250 }
+];
+
+const columns = columnsDraft.map((col) => {
+  if (col.field === 'col1') {
+    return col;
+  }
+
+  return {
+    ...col,
+    renderHeader(params: GridColumnHeaderParams<any, any, any>) {
+      return (
+        <strong className='font-semibold'>
+          {params.colDef.headerName} 🔑{' '}
+        </strong>
+      );
+    }
+  };
+});
