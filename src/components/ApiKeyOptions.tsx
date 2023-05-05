4fe2a63533e0fe00ba8c0f6b9dd5ea13e55a5d20
Emiko Esther S <graceupongraceemi@gmail.com>
2023-05-05 12:43:59 -0700

    In ApiKeyOptions.tsx, state added for DropdownMenu

--- src/components/ApiKeyOptions.tsx
+++ src/components/ApiKeyOptions.tsx
@@ -1,9 +1,30 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState } from 'react';
+import { DropdownMenu, DropdownMenuTrigger } from '@/ui/DropdownMenu';
+import { Button } from './ui/Button';
 
 interface ApiKeyOptionsProps {}
 
 const ApiKeyOptions: FC<ApiKeyOptionsProps> = ({}) => {
-  return <div>ApiKeyOptions</div>;
+  const [isCreatingNew, setIsCreatingNew] = useState<boolean>(false);
+  const [isRevoking, setIsRevoking] = useState<boolean>(false);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger disabled={isCreatingNew || isRevoking} asChild>
+        <Button variant='ghost' className='flex gap-2 items-center'>
+          <p>
+            {isCreatingNew
+              ? 'Creating new key'
+              : isRevoking
+              ? 'Revoking key'
+              : 'Options'}
+          </p>
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
 
 export default ApiKeyOptions;
