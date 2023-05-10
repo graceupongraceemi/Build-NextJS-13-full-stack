fdcd2c49a6551106a88e098f6b2d1d6472967085
Emiko Esther S <graceupongraceemi@gmail.com>
2023-05-10 14:58:03 -0700

    In similarity.ts, apiKey function added. In lib directory, openai.ts file created

--- src/pages/api/v1/similarity.ts
+++ src/pages/api/v1/similarity.ts
@@ -9,6 +9,42 @@ const reqShema = z.object({
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const body = req.body as unknown;
+
+  const apiKey = req.headers.authorization
+  if(!apiKey) {
+    return res.status(401).json({error: 'Unauthorized'})
+  }
+
+
+  try {
+    const {text1, text2} = reqShema.parse(body)
+
+    const validApiKey = await db.apiKey.findFirst({
+      where: {
+        key: apiKey,
+        enabled: true,
+      }
+    })
+
+    if(!validApiKey) {
+      return res.status(401).json({ error: 'Unauthorized'})
+    }
+  
+    const start = new Date()
+
+    const embedding = await Prommise.all(
+      [text1, text2].map(async (text) => {
+          const res = await 
+      })
+    )
+
+  } catch (error) {
+    if(error instanceof z.ZodError) {
+      return res.status(400).json({ error: error.issues })
+    }
+
+    return res.status(500).json({ error: 'Internal Server Error'})
+  }
 };
 
 export default withMethods(['POST'], handler);
