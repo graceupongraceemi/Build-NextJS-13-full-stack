816e7f802b20ccec3f34a69fbb76f84297b8e890
Emiko Esther S <graceupongraceemi@gmail.com>
2023-05-15 21:36:21 -0700

    In middleware.ts, redis url and token are added along with ratelimit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,25 @@
-import { Redis } from '@upstash/redis'
-import { withAuth } from 'next-auth/middleware'
-
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
+import { withAuth } from 'next-auth/middleware';
 
 const redis = new Redis({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
   token: process.env.REDIS_SECRET
-})
+});
 
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(5, '1 h')
+});
 
-export default withAuth(
-  async function middleware(req) {
-    const pathname = req.nextUrl.pathname // relative path
+export default withAuth(async function middleware(req) {
+  const pathname = req.nextUrl.pathname; // relative path
 
-    // Manage rate limiting
-    if(pathname.startsWith('/api')) {
-      const ip = req.ip ?? '127.0.0.1'
-      try {
-          const { success } = await ratelimit.limit(ip)
-      } catch (error) {
-
-      }
-    }
-
+  // Manage rate limiting
+  if (pathname.startsWith('/api')) {
+    const ip = req.ip ?? '127.0.0.1';
+    try {
+      const { success } = await ratelimit.limit(ip);
+    } catch (error) {}
   }
-)
+});
