3cc63799ca591d6f930d9a704df5323e49a78254
Emiko Esther S <graceupongraceemi@gmail.com>
2023-05-17 21:49:55 -0700

    UserAuthForm.tsx changed to use client. loginWithGoogle async function added

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,7 +1,24 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState } from 'react';
+import { Button } from './ui/Button';
+import { cn } from '@/lib/utils';
 
 const UserAuthForm: FC = () => {
-  return <div>UserAuthForm</div>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+    } catch (error) {}
+  };
+
+  return (
+    <div className={cn('flex justify-center')}>
+      <Button isLoading></Button>
+    </div>
+  );
 };
 
 export default UserAuthForm;
